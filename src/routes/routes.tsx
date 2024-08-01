23f3456d167ec980394967ed0d2516cb0e3eaf74
Alesia Klemenia <klemeny_ap_20@mf.grsu.by>
2024-08-01 23:52:33 +0300

    feat: add ErrorBoundary

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -3,12 +3,14 @@ import { createBrowserRouter } from "react-router-dom";
 
 import paths from "@/constants/paths";
 
+const ErrorBoundary = lazy(() => import("@components/ErrorBoundary"));
 const Home = lazy(() => import("@/pages/Home"));
 
 const router = createBrowserRouter([
   {
     path: paths.home,
     element: <Home />,
+    errorElement: <ErrorBoundary />,
   },
 ]);
 
